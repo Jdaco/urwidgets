9168030b48f0c4dae696640d38e200db8e40543f
Chaise Conn <chaisecanz@gmail.com>
2014-07-22 11:13:53 -0700

    Added version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup_args = dict(
     name="urwidgets",
-    version="",
+    version="0.1",
     description="A collection of useful widgets to use with the urwid library",
     author="Chaise Conn",
     author_email="chaisecanz@gmail.com",
